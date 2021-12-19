c0974e56aec7492b7d8d9c5453f5c676682b1136
Swarnika Shakya <sswarnika@tbc.edu.np>
2021-12-19 10:29:30 +0545

    Activity name set

---
+++ app/src/main/java/com/example/android_todo_first/Todo.java
@@ -0,0 +1,65 @@
+package com.example.android_todo_first;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Todo {
+
+    private UUID id;
+    private String title;
+    private String description;
+    private int priority;
+    private Date updatedAt;
+
+    public Todo(UUID id, String title, String description, int priority, Date updatedAt) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+        this.updatedAt = updatedAt;
+    }
+
+    public Todo(){
+        id=UUID.randomUUID();
+        updatedAt=new Date();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
